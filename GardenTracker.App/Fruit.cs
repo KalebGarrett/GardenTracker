baa5fcfd87a1b899b56dabcb1a3a0fd0f9d44d95
Kaleb Garrett <kalebagarrett@gmail.com>
2023-09-05 16:33:51 -0400

    Added fruit class

---
+++ GardenTracker.App/Fruit.cs
@@ -0,0 +1,25 @@
+﻿using GardenTracker.App.Interfaces;
+
+namespace GardenTracker.App;
+
+public class Fruit : IPlant
+{
+    public Fruit(string plantType, string commonName, string scientificName, string description, string plantingDate, string wateringSchedule, string ripeningTime)
+    {
+        PlantType = plantType;
+        CommonName = commonName;
+        ScientificName = scientificName;
+        Description = description;
+        PlantingDate = plantingDate;
+        WateringSchedule = wateringSchedule;
+        RipeningTime = ripeningTime;
+    }
+
+    public string PlantType { get; set; }
+    public string CommonName { get; set; }
+    public string ScientificName { get; set; }
+    public string Description { get; set; }
+    public string PlantingDate { get; set; }
+    public string WateringSchedule { get; set; }
+    public string RipeningTime { get; set; }
+}
