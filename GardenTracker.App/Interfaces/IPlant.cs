debe31f50881fc6a8acdeef1fcea347e934f9d43
Kaleb Garrett <kalebagarrett@gmail.com>
2023-09-06 20:09:20 -0400

    Updated interface

--- GardenTracker.App/Interfaces/IPlant.cs
+++ GardenTracker.App/Interfaces/IPlant.cs
@@ -2,10 +2,11 @@
 
 public interface IPlant
 {
+    int Id { get; set; }
     string PlantType { get; set; }
     string CommonName { get; set; }
     string ScientificName { get; set; }
     string Description { get; set; }
     string PlantingDate { get; set; }
-    string WateringSchedule { get; set; }
+    int WateringSchedule { get; set; }
 }
