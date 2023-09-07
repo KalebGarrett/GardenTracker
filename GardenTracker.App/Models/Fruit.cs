cc4166bdd589043e1f82791f672b7d52979e8291
Kaleb Garrett <kalebagarrett@gmail.com>
2023-09-06 20:09:08 -0400

    Added models folder

---
+++ GardenTracker.App/Models/Fruit.cs
@@ -0,0 +1,28 @@
+﻿using GardenTracker.App.Interfaces;
+
+namespace GardenTracker.App.Models;
+
+public class Fruit : IPlant
+{
+    public Fruit(int id, string plantType, string commonName, string scientificName, 
+        string description, string plantingDate, int wateringSchedule, int ripeningTime)
+    {
+        Id = id;
+        PlantType = plantType;
+        CommonName = commonName;
+        ScientificName = scientificName;
+        Description = description;
+        PlantingDate = plantingDate;
+        WateringSchedule = wateringSchedule;
+        RipeningTime = ripeningTime;
+    }
+
+    public int Id { get; set; }
+    public string PlantType { get; set; }
+    public string CommonName { get; set; }
+    public string ScientificName { get; set; }
+    public string Description { get; set; }
+    public string PlantingDate { get; set; }
+    public int WateringSchedule { get; set; }
+    public int RipeningTime { get; set; }
+}
---
+++ GardenTracker.App/Models/Vegetable.cs
@@ -0,0 +1,28 @@
+﻿using GardenTracker.App.Interfaces;
+
+namespace GardenTracker.App.Models;
+
+public class Vegetable : IPlant
+{
+    public Vegetable(int id, string plantType, string commonName, string scientificName, 
+        string description, string plantingDate, int wateringSchedule, double sowingDepth)
+    {
+        Id = id;
+        PlantType = plantType;
+        CommonName = commonName;
+        ScientificName = scientificName;
+        Description = description;
+        PlantingDate = plantingDate;
+        WateringSchedule = wateringSchedule;
+        SowingDepth = sowingDepth;
+    }
+
+    public int Id { get; set; }
+    public string PlantType { get; set; }
+    public string CommonName { get; set; }
+    public string ScientificName { get; set; }
+    public string Description { get; set; }
+    public string PlantingDate { get; set; }
+    public int WateringSchedule { get; set; }
+    public double SowingDepth { get; set; }
+}
