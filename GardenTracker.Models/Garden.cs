85fbad4628d51e801141f815cbca387cf3d0a894
Kaleb Garrett <kalebagarrett@gmail.com>
2023-09-12 16:52:23 -0400

    Added class library

---
+++ GardenTracker.Models/Garden.cs
@@ -0,0 +1,12 @@
+﻿namespace GardenTracker.Models;
+
+public class Garden
+{
+    public int Id { get; set; }
+    public string PlantType { get; set; }
+    public string CommonName { get; set; }
+    public string ScientificName { get; set; }
+    public string Description { get; set; }
+    public string PlantingDate { get; set; }
+    public int WateringSchedule { get; set; }
+}
